f29b932a3208a31df40c30de2ff73e9ae5bd1406
quiluc <quiluc@host.docker.internal>
2022-09-14 20:09:25 +0700

    add missing file

---
+++ src/test/java/com/sentifi/microservice/service/PriceServiceIT.java
@@ -0,0 +1,30 @@
+package com.sentifi.microservice.service;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.sentifi.microservice.MicroserviceApplication;
+import com.sentifi.microservice.model.ClosePriceData;
+
+@SpringBootTest(classes = MicroserviceApplication.class)
+public class PriceServiceIT {
+
+  @Autowired
+  private PriceService priceService;
+
+  @Test
+  public void testFindClosePriceForFB() throws ParseException {
+    ClosePriceData closePrice = priceService.findClosePrice("FB",
+        new SimpleDateFormat("yyyy-MM-dd").parse("2015-01-01"),
+        new SimpleDateFormat("yyyy-MM-dd").parse("2015-01-31"));
+    assertNotNull(closePrice);
+    assertNotNull(closePrice.getInfo());
+  }
+
+}
